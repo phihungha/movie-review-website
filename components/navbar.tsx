6e68d9d4a6591ab5155b9ddc10f97964febc4108
Ha Phi Hung <haphihung55@gmail.com>
2023-06-15 22:32:18 +0700

    Add home navbar link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function NavBar() {
   return (
     <nav className="flex gap-5 justify-end">
+      <Link href="/">Home</Link>
       <Link href="/movies">Movies</Link>
       <Link href="/users">Users</Link>
       <Link href="/personal">My Account</Link>
