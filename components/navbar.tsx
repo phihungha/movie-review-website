ad5ad10b65c96540c3e95b1ce620d8d701e12513
Ha Phi Hung <haphihung55@gmail.com>
2023-06-15 22:31:27 +0700

    Setup basic navigation

---
+++ components/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="flex gap-5 justify-end">
+      <Link href="/movies">Movies</Link>
+      <Link href="/users">Users</Link>
+      <Link href="/personal">My Account</Link>
+      <Link href="/signup">Sign up</Link>
+      <Link href="/signin">Sign in</Link>
+    </nav>
+  );
+}
