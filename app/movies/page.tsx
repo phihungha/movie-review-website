17921ccb38fe3b78afe38d8b2b9903ae02623b44
Ha Phi Hung <haphihung55@gmail.com>
2023-06-17 20:17:38 +0700

    Remove test code

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,42 +1,7 @@
-import LoadingSpinner from "@/components/LoadingSpinner";
-import { Movie, fetchMovies } from "@/lib/fetch-movies";
-import Image from "next/image";
-import { Suspense } from "react";
-
 export const metadata = {
   title: "Movies",
 };
 
-async function MovieList() {
-  const movies = await fetchMovies();
-  return (
-    <div className="flex gap-10">
-      {movies.map((i: Movie) => (
-        <div key={i.id}>
-          <Image
-            key={i.id}
-            width={150}
-            height={150}
-            src={i.posterUrl}
-            alt={`Movie poster for ${i.title}`}
-          />
-          <p>Title: {i.title}</p>
-          <p>Release date: {i.releaseDate}</p>
-          <p>Critic score: {i.criticScore}</p>
-          <p>Regular score: {i.regularScore}</p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default async function Movies() {
-  return (
-    <div>
-      <p>Movies</p>
-      <Suspense fallback={<LoadingSpinner />}>
-        <MovieList />
-      </Suspense>
-    </div>
-  );
+  return <p>Movies</p>;
 }
--- lib/api-url.ts
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://cinerate-web.05052023.xyz";
-
-export function apiUrl(relativePath: string) {
-  return API_URL.concat(relativePath);
-}
