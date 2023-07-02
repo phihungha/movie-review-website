3f71bb4fe90e82d7029acc5087fe48bd6d18cdb6
Ha Phi Hung <haphihung55@gmail.com>
2023-07-02 21:42:10 +0700

    Integrate homepage to api

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,16 @@
-"use client";
 import MovieCard, { MovieData } from "@/components/MovieCard";
+import { serverFetch } from "./lib/server-fetch";
 
-export default function Home() {
-  const movies: MovieData[] = [];
+export default async function Home() {
+  const trendingMovies: MovieData[] = await serverFetch("/trendingMovies");
+  const recentMovies: MovieData[] = await serverFetch("/recentMovies");
   return (
     <div>
       <h2 className="text-2xl pt-0 px-20 not-italic font-bold text-gray-900">
         Popular movies
       </h2>
       <div className="flex flex-row pt-10 pb-10 px-20 gap-20 w-full overflow-auto">
-        {movies.map((movie: MovieData) => (
+        {trendingMovies.map((movie: MovieData) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
@@ -17,7 +18,7 @@ export default function Home() {
         Recently Release
       </h2>
       <div className="grid place-items-center grid-cols-4 p-5 gap-10 justify-center">
-        {movies.map((movie: MovieData) => (
+        {recentMovies.map((movie: MovieData) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import StarIcon from "@mui/icons-material/Star";
