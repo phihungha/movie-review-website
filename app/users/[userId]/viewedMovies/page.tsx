5bbcf0990e300db55ef01c666a43369d35225d23
Trung <20520333@ms.uit.edu.vn>
2023-07-03 17:09:54 +0700

    integrate user reviews, movies

--- app/users/[userId]/viewedMovies/page.tsx
+++ app/users/[userId]/viewedMovies/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import MovieCard, { MovieData } from "@/components/MovieCard";
 import * as React from "react";
+import useSWR from "swr";
 
-const movies: MovieData[] = [];
+interface UserDetailsProps {
+  params: {
+    userId: string;
+  };
+}
 
-export default function UserWatchedMovies() {
+export default function UserWatchedMovies({ params }: UserDetailsProps) {
+  const { data, error } = useSWR(`/users/${params.userId}/viewed-movies`);
   return (
     <div className="grid place-items-center grid-cols-4 p-5 gap-10 justify-center">
-      {movies.map((movie: MovieData) => (
+      {data?.map((movie: MovieData) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </div>
