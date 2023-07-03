f1a55a75171e8d839f4facb6767a5d0e02a35e07
Trung <20520333@ms.uit.edu.vn>
2023-07-03 17:01:53 +0700

    fix ReviewCard

--- components/ReviewCard.tsx
+++ components/ReviewCard.tsx
@@ -4,43 +4,54 @@ import React from "react";
 import StarIcon from "@mui/icons-material/Star";
 import CrewCard, { ICrewCard } from "@/components/CrewCard";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import useSWR from "swr";
 
 export interface IReviewCard {
   id: number;
-  user: ICrewCard;
-  review_title: string;
-  description: string;
+  authorId: string;
+  movieId: number;
+  title: string;
+  content: string;
   score: number;
-  date: string;
-  like: number;
+  postTime: string;
+  thankCount: number;
 }
 
 const ReviewCard = ({ review }: { review: IReviewCard }) => {
+  const link = "/movies/" + review.movieId + "/reviews/" + review.id;
+  const { data, error } = useSWR(`/users/${review.authorId}`);
+  const formatscore = review.score / 2;
+  const user: ICrewCard = {
+    id: data?.id,
+    Name: data?.username,
+    role: data?.userType,
+    avatar_path: data?.avatarUrl,
+  };
   return (
-    <Link href="/movies/2/reviews/2">
-      <div className="flex flex-row p-0">
-        <CrewCard user={review.user}></CrewCard>
-        <div className="flex flex-col items-start pl-3">
-          <h2 className="w-24 h-6 not-italic font-bold text-base leading-6 text-black-100">
-            {review.review_title}
+    <Link href={link}>
+      <div className="flex flex-row nowrap p-0">
+        <CrewCard user={user}></CrewCard>
+        <div className="flex flex-col w-full items-start pl-3">
+          <h2 className="w-full h-6 not-italic font-bold text-base nowrap leading-6 text-black-100">
+            {review.title}
           </h2>
           <div className="flex flex-row items-center gap-10 p-0 h-6">
             <div className="flex flex-row items-center w-10 h-5 gap-1">
               <StarIcon color="warning" />
               <h2 className="w-12 h-4 not-italic font-normal text-xs leading-4 text-black-500">
-                {review.score}
+                {formatscore}
               </h2>
             </div>
             <h2 className="not-italic font-normal text-xs leading-4 text-black-500">
-              {review.date}
+              {review.postTime}
             </h2>
           </div>
           <h2 className="not-italic font-normal text-xs text-black-100">
-            {review.description}
+            {review.content}
           </h2>
           <button className="not-italic p-2 items-center font-normal text-xs leading-4 text-black-500">
             <ThumbUpOffAltIcon fontSize="small" color="primary" />
-            {review.like} likes
+            {review.thankCount} thanks
           </button>
         </div>
       </div>
