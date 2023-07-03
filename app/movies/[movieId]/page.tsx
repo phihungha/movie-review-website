44b5eab520549b1ec4cc21d7187185aabc75f9f7
Trung <20520333@ms.uit.edu.vn>
2023-07-03 17:05:20 +0700

    integrate moviedetail

--- app/movies/[movieId]/page.tsx
+++ app/movies/[movieId]/page.tsx
@@ -53,58 +53,31 @@ const sampleuser: ICrewCard = {
   avatar_path: "",
 };
 
-const reviews: IReviewCard[] = [
-  {
-    id: 1,
-    user: sampleuser,
-    review_title: "Review Title",
-    description:
-      "At last, the chief of police assigns “crime expert” Dizdar Koşu, who has recently come back from the US, to the team.Overwhelmed with panic, pressure and disdain, Emin and his friends try to solve the strangest series of murders with the most unusual methods.This fast race against time immediately turns into a tragicomedy.",
-    score: 4.5,
-    date: "2023-03-25",
-    like: 1000,
-  },
-  {
-    id: 2,
-    user: sampleuser,
-    review_title: "Review Title",
-    description:
-      "At last, the chief of police assigns “crime expert” Dizdar Koşu, who has recently come back from the US, to the team.Overwhelmed with panic, pressure and disdain, Emin and his friends try to solve the strangest series of murders with the most unusual methods.This fast race against time immediately turns into a tragicomedy.",
-    score: 4.5,
-    date: "2023-03-25",
-    like: 1000,
-  },
-  {
-    id: 3,
-    user: sampleuser,
-    review_title: "Review Title",
-    description:
-      "At last, the chief of police assigns “crime expert” Dizdar Koşu, who has recently come back from the US, to the team.Overwhelmed with panic, pressure and disdain, Emin and his friends try to solve the strangest series of murders with the most unusual methods.This fast race against time immediately turns into a tragicomedy.",
-    score: 4.5,
-    date: "2023-03-25",
-    like: 1000,
-  },
-  {
-    id: 4,
-    user: sampleuser,
-    review_title: "Review Title",
-    description:
-      "At last, the chief of police assigns “crime expert” Dizdar Koşu, who has recently come back from the US, to the team.Overwhelmed with panic, pressure and disdain, Emin and his friends try to solve the strangest series of murders with the most unusual methods.This fast race against time immediately turns into a tragicomedy.",
-    score: 4.5,
-    date: "2023-03-25",
-    like: 1000,
-  },
-];
-
 interface MovieDetailsProps {
   params: {
     movieId: string;
   };
 }
 
+function MovieReviews({ params }: MovieDetailsProps) {
+  const { data, error } = useSWR(`/movies/${params.movieId}/reviews`);
+  return (
+    <div className="grid place-items-start grid-rows-1 pb-10 pt-10 gap-10 px-20 justify-start">
+      {data?.map((review: IReviewCard) => (
+        <ReviewCard key={review.id} review={review} />
+      ))}
+    </div>
+  );
+}
+
+function HandleSubmit() {
+  console.log("api here!");
+  return;
+}
+
 export default function MovieDetails({ params }: MovieDetailsProps) {
   const { data, error } = useSWR(`/movies/${params.movieId}`);
-
+  const link = "/movies/" + params.movieId + "/reviews";
   return (
     <div className="place-items-center content-center">
       <div className="flex whitespace-nowrap flex-row p-0 w-full">
@@ -114,7 +87,7 @@ export default function MovieDetails({ params }: MovieDetailsProps) {
             src={data?.posterUrl}
             width={240}
             height={380}
-            alt={"John Wick"}
+            alt={data?.title}
           />
           <Rating size="large" name="read-only" value={4.5} readOnly />
         </div>
@@ -130,24 +103,7 @@ export default function MovieDetails({ params }: MovieDetailsProps) {
           </div>
           <div>
             <h2 className="text-lg not-italic font-normal leading-4 text-black-100 whitespace-pre-line">
-              Istanbul Police DepartmentIstanbul Police DepartmentIstanbul
-              Police DepartmentIstanbul Police DepartmentIstanbul Police
-              DepartmentIstanbul Police DepartmentIstanbul Police
-              DepartmentIstanbul Police DepartmentIstanbul Police
-              DepartmentIstanbul Police DepartmentIstanbul Police
-              DepartmentIstanbul Police DepartmentIstanbul Police
-              DepartmentIstanbul Police Department homicide detectives encounter
-              a murder nothing like they’ve seen before.Commissioners Emin,
-              Salih, Asuman and deputy Alaattin begin investigating.But strange
-              murders keep happening. They don’t have any evidence or any clues.
-              Day by day, tempers flare and the police chiefs grow annoyed.As
-              the murders keep piling up, the public’s interest rises. The
-              pressure on Emin and his colleagues get intense. At last, the
-              chief of police assigns “crime expert” Dizdar Koşu, who has
-              recently come back from the US, to the team.Overwhelmed with
-              panic, pressure and disdain, Emin and his friends try to solve the
-              strangest series of murders with the most unusual methods.This
-              fast race against time immediately turns into a tragicomedy.
+              {data?.synopsis}
             </h2>
           </div>
           <Divider className="w-full" />
@@ -182,28 +138,21 @@ export default function MovieDetails({ params }: MovieDetailsProps) {
           />
           <div className="flex flex-row items-center gap-10 p-0 h-6">
             <Rating size="medium" value={4.5} />
-            <button className="not-italic p-2 items-center font-normal text-xs leading-4 text-gray-500">
-              <ThumbUpOffAltIcon fontSize="medium" color="primary" />
-            </button>
           </div>
         </div>
       </div>
       <div className="px-20 pt-10 items-end text-end">
-        <Button className="px-20" variant="outlined">
+        <Button className="px-20" variant="outlined" onClick={HandleSubmit}>
           Submit
         </Button>
       </div>
       <h2 className="text-2xl pt-20 px-20 not-italic font-bold text-gray-900">
         Reviews
       </h2>
-
-      <div className="grid place-items-center grid-rows-1 pb-10 pt-10 px-20 gap-10 justify-center">
-        {reviews.map((review: IReviewCard) => (
-          <ReviewCard key={review.id} review={review} />
-        ))}
+      <div className="w-full">
+        <MovieReviews params={params} />
       </div>
-
-      <Link href="/movies/2/reviews">
+      <Link href={link}>
         <h2 className="text-xl pt-0 px-20 pb-20 text-center not-italic font-bold text-gray-900">
           All Reviews
         </h2>
