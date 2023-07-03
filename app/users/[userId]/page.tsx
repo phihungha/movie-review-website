c21e5eb78d8700cc00d115ced2d06c6dcd64fbef
Trung <20520333@ms.uit.edu.vn>
2023-07-03 17:11:25 +0700

    intergrate user detail

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -4,20 +4,33 @@ import Divider from "@mui/material/Divider";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import useSWR from "swr";
 
-export default function UserDetails() {
+interface UserDetailsProps {
+  params: {
+    userId: string;
+  };
+}
+
+export default function UserDetails({ params }: UserDetailsProps) {
+  const { data, error } = useSWR(`/users/${params.userId}`);
+  const linkAll = "/users/" + params.userId + "/reviews";
+  const linkThanked = "/users/" + params.userId + "/thankedReviews";
+  const linkViewed = "/users/" + params.userId + "/viewedMovies";
   return (
     <div className="items-center justify-center py-20">
-      <div className="flex place-items-center flex-row px-20 w-full">
+      <div className="flex place-items-center flex-row py-20 px-20 w-full">
         <div className="flex flex-col text-center place-items-center px-20 gap-5">
           <Avatar
             className="items-center text-center w-16 h-16"
-            src=""
-            alt="Trung"
+            src={data?.avatarUrl}
+            alt={data?.name}
             sx={{ width: 240, height: 380 }}
           />
-          <h2 className="text-3xl not-italic font-bold text-gray-900">Name</h2>
-          <h2 className="text-xl not-italic text-gray-900">Critic</h2>
+          <h2 className="text-3xl not-italic font-bold text-gray-900">
+            {data?.username}
+          </h2>
+          <h2 className="text-xl not-italic text-gray-900">{data?.userType}</h2>
         </div>
         <Divider orientation="vertical" className="w-5" flexItem />
         <div className="flex flex-col items-end  w-9/12 px-20 gap-3">
@@ -26,17 +39,23 @@ export default function UserDetails() {
               <TextField
                 className="w-full p-0 not-italic font-bold text-base leading-6 text-gray-100"
                 label="Email"
-                defaultValue="20520333@gm.uit.edu.vn"
+                defaultValue={data?.email}
                 variant="filled"
+                InputProps={{
+                  readOnly: true,
+                }}
               />
             </div>
 
             <div className="px-10 py-5">
               <TextField
                 className="w-full p-0 not-italic font-bold text-base leading-6 text-gray-100"
-                label="Name"
-                defaultValue="Trung"
+                label="User Name"
+                defaultValue={data?.name}
                 variant="filled"
+                InputProps={{
+                  readOnly: true,
+                }}
               />
             </div>
           </div>
@@ -45,39 +64,44 @@ export default function UserDetails() {
               <TextField
                 className="w-full p-0 not-italic font-bold text-base leading-6 text-gray-100"
                 label="Birthday"
-                defaultValue="26-10-2002"
+                defaultValue={data?.dateOfBirth}
                 variant="filled"
+                InputProps={{
+                  readOnly: true,
+                }}
               />
             </div>
 
             <div className="px-10 py-5">
               <TextField
                 className="w-full p-0 not-italic font-bold text-base leading-6 text-gray-100"
-                label="Favorite Genre"
-                defaultValue="Action"
+                label="Gender"
+                defaultValue={data?.gender}
                 variant="filled"
+                InputProps={{
+                  readOnly: true,
+                }}
               />
             </div>
           </div>
         </div>
-        <Divider orientation="vertical" flexItem />
       </div>
       <div className="flex flex-row justify-center w-full gap-10 p-20 h-6">
-        <Link href="/users/1/reviews">
+        <Link href={linkAll}>
           <div className="self-center">
             <Button className="px-20" variant="outlined">
               All Reviews
             </Button>
           </div>
         </Link>
-        <Link href="/users/1/thankedReviews">
+        <Link href={linkThanked}>
           <div className="self-center">
             <Button className="px-20" variant="outlined">
               Thanked Reviews
             </Button>
           </div>
         </Link>
-        <Link href="/users/1/viewedMovies">
+        <Link href={linkViewed}>
           <div className="self-center">
             <Button className="px-20" variant="outlined">
               Viewed Movies
