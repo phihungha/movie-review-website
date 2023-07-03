530d5cc730faa28efe679974723594c7c76b9d02
Trung <20520333@ms.uit.edu.vn>
2023-07-03 17:06:40 +0700

    integrate ReviewDetail

--- app/movies/[movieId]/reviews/[reviewId]/page.tsx
+++ app/movies/[movieId]/reviews/[reviewId]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 import Image from "next/image";
+import { ICrewCard } from "@/components/CrewCard";
+import { MovieData } from "@/components/MovieCard";
 import Avatar from "@mui/material/Avatar";
 import Divider from "@mui/material/Divider";
 import Rating from "@mui/material/Rating";
@@ -7,71 +9,113 @@ import TextField from "@mui/material/TextField";
 import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import Button from "@mui/material/Button";
 import Link from "next/link";
+import useSWR from "swr";
 
-export default function ReviewDetails({
-  params,
-}: {
-  params: { slug: string };
-}) {
+function HandleSave() {
+  console.log("api here!");
+  return;
+}
+
+function HandleDelete() {
+  console.log("api here!");
+  return;
+}
+
+interface ReviewDetailsProps {
+  params: {
+    reviewId: string;
+  };
+}
+function GetUser(userId: string): ICrewCard {
+  const { data, error } = useSWR(`/users/${userId}`);
+  const user: ICrewCard = {
+    id: data?.id,
+    Name: data?.username,
+    role: data?.userType,
+    avatar_path: data?.avatarUrl,
+  };
+  return user;
+}
+
+function GetMovie(movieId: string): MovieData {
+  const { data, error } = useSWR(`/movies/${movieId}`);
+  const movie: MovieData = {
+    id: data?.id,
+    title: data?.title,
+    releaseYear: data?.releaseYear,
+    posterUrl: data?.posterUrl,
+    criticScore: data?.criticScore,
+    regularScore: data?.regularScore,
+  };
+  return movie;
+}
+
+export default function ReviewDetails({ params }: ReviewDetailsProps) {
+  const { data, error } = useSWR(`/reviews/${params.reviewId}`);
+  const user = GetUser(data?.authorId);
+  const movie = GetMovie(data?.movieId);
+  const formatScore = data?.score / 2;
   return (
     <div>
       <div className="flex whitespace-nowrap flex-row p-0 w-full">
         <div className="flex flex-col text-center place-items-center p-20 gap-10">
           <Image
             className="rounded"
-            src="https://static.wikia.nocookie.net/john-wick8561/images/7/7c/Chapterfour.jpeg/revision/latest?cb=20230523074638"
+            src={movie.posterUrl}
             width={240}
             height={380}
-            alt={"John Wick"}
+            alt={movie.title}
           />
           <h2 className="text-3xl not-italic font-bold text-gray-900">
-            John Wick
+            {movie.title}
           </h2>
         </div>
         <div className="flex flex-col items-start w-9/12 p-20 gap-3 ">
           <div className="flex whitespace-nowrap flex-row p-0">
             <Avatar
               className="items-center text-center w-16 h-16"
-              src=""
-              alt="Trung"
+              src={user.avatar_path}
+              alt={user.Name}
               sx={{ width: 30, height: 30 }}
             />
             <div className="flex flex-col justify-center items-center gap-1">
               <h2 className="w-24 h-5 not-italic font-normal items-center text-center text-base leading-6 text-black-100">
-                Trung
+                {user.Name}
               </h2>
               <h2 className="w-12 h-4 not-italic font-normal items-center text-center text-xs leading-4 text-black-500">
-                Critic
+                {user.role}
               </h2>
             </div>
           </div>
           <Divider className="w-full" />
           <TextField
             className="w-full not-italic font-bold text-base leading-6 text-gray-100"
-            label="Review Title"
+            value={data?.title}
           />
           <TextField
             className="w-full"
             id="description"
-            label="Description"
+            value={data?.content}
             multiline
             rows={4}
           />
           <div className="flex flex-row items-center gap-10 p-0 h-6">
-            <Rating size="medium" value={4.5} />
-            <button className="not-italic p-2 items-center font-normal text-xs leading-4 text-gray-500">
-              <ThumbUpOffAltIcon fontSize="medium" color="primary" />
-            </button>
+            <Rating size="medium" value={formatScore} />
           </div>
           <div className="flex flex-row justify-center w-full gap-10 pt-10 h-6">
             <div className="self-center">
-              <Button color="error" className="px-20" variant="outlined">
+              <Button
+                color="error"
+                className="px-20"
+                variant="outlined"
+                onClick={HandleDelete}
+              >
                 Delete
               </Button>
             </div>
             <div className="self-center">
-              <Button className="px-20" variant="outlined">
-                Submit
+              <Button className="px-20" variant="outlined" onClick={HandleSave}>
+                Save
               </Button>
             </div>
           </div>
