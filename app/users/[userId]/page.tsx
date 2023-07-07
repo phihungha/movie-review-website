0d97841300a228004d53c73bb7883db7adc06f52
Ha Phi Hung <haphihung55@gmail.com>
2023-07-08 04:35:16 +0700

    Fix user details layout

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -8,6 +8,7 @@ import Button from "@mui/material/Button";
 import Link from "next/link";
 import useSWR from "swr";
 import { ReviewData } from "@/types/ReviewData";
+import SectionText from "@/components/Texts/SectionText";
 
 interface UserDetailsProps {
   params: {
@@ -17,12 +18,10 @@ interface UserDetailsProps {
 
 export default function UserDetails({ params }: UserDetailsProps) {
   const { data } = useSWR(`/users/${params.userId}`);
-  const linkAll = "/users/" + params.userId + "/reviews";
-  const linkThanked = "/users/" + params.userId + "/thankedReviews";
-  const linkViewed = "/users/" + params.userId + "/viewedMovies";
+  const userId = params.userId;
 
   return (
-    <div className="items-center justify-center py-20">
+    <div className="items-center justify-center p-20">
       <div className="flex w-full flex-row place-items-center px-20 py-20">
         <div className="flex flex-col place-items-center gap-5 px-20 text-center">
           <Avatar
@@ -37,8 +36,8 @@ export default function UserDetails({ params }: UserDetailsProps) {
           <h2 className="text-xl not-italic text-gray-900">{data?.userType}</h2>
         </div>
         <Divider orientation="vertical" className="w-5" flexItem />
-        <div className="flex w-9/12 flex-col  items-center gap-3 px-20">
-          <div className="w-full px-10">
+        <div className="flex w-9/12 flex-col items-center gap-3 px-20">
+          <div className="w-full px-10 py-5">
             <TextField
               className="w-full p-0 text-base font-bold not-italic leading-6 text-gray-100"
               label="Email"
@@ -79,47 +78,37 @@ export default function UserDetails({ params }: UserDetailsProps) {
         </div>
       </div>
 
-      <h2 className="px-20 pt-10 text-2xl font-bold not-italic text-gray-900">
-        Reviews
-      </h2>
-      <div className="grid grid-rows-1 place-items-start justify-start gap-10 px-20 pb-10 pt-10">
-        {data?.reviews.map((review: ReviewData) => (
-          <ReviewCard key={review.id} review={review} />
-        ))}
-      </div>
-      <Link href={linkAll}>
-        <h2 className="px-20 pb-20 pt-0 text-center text-xl font-bold not-italic text-gray-900">
-          All Reviews
-        </h2>
-      </Link>
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-7">
+          <SectionText>Reviews</SectionText>
+          {data?.reviews.map((review: ReviewData) => (
+            <ReviewCard key={review.id} review={review} />
+          ))}
+          <Link href={`/users/${userId}/reviews`}>
+            <Button>All reviews...</Button>
+          </Link>
+        </div>
 
-      <h2 className="px-20 pt-10 text-2xl font-bold not-italic text-gray-900">
-        Thanked Reviews
-      </h2>
-      <div className="grid grid-rows-1 place-items-start justify-start gap-10 px-20 pb-10 pt-10">
-        {data?.reviewThanks.map((review: ReviewData) => (
-          <ReviewCard key={review.id} review={review} />
-        ))}
-      </div>
-      <Link href={linkThanked}>
-        <h2 className="px-20 pb-20 pt-0 text-center text-xl font-bold not-italic text-gray-900">
-          All Reviews
-        </h2>
-      </Link>
+        <div className="flex flex-col gap-7">
+          <SectionText>Thanked reviews</SectionText>
+          {data?.reviewThanks.map((review: ReviewData) => (
+            <ReviewCard key={review.id} review={review} />
+          ))}
+          <Link href={`/users/${userId}/thankedReviews`}>
+            <Button>All thanked reviews...</Button>
+          </Link>
+        </div>
 
-      <h2 className="px-20 pt-10 text-2xl font-bold not-italic text-gray-900">
-        Viewed Movies
-      </h2>
-      <div className="grid grid-rows-1 place-items-start justify-start gap-10 px-20 pb-10 pt-10">
-        {data?.viewedMovies.map((movie: MovieData) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        <div className="flex flex-col gap-7">
+          <SectionText>Watched movies</SectionText>
+          {data?.viewedMovies.map((movie: MovieData) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
+          <Link href={`/users/${userId}/viewedMovies`}>
+            <Button>All watched movies...</Button>
+          </Link>
+        </div>
       </div>
-      <Link href={linkViewed}>
-        <h2 className="px-20 pb-20 pt-0 text-center text-xl font-bold not-italic text-gray-900">
-          All Movies
-        </h2>
-      </Link>
     </div>
   );
 }
