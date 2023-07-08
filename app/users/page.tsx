6f77693bacd2b38fa754d6df16c6aaf19d401ce7
Ha Phi Hung <haphihung55@gmail.com>
2023-07-08 10:22:44 +0700

    Implement search user

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,16 +1,29 @@
 "use client";
+
 import useSWR from "swr";
 import UserCard, { IUserCard } from "@/components/UserCard";
 import * as React from "react";
+import TextField from "@mui/material/TextField";
 
 //export const metadata = {
 //  title: "Users",
 //};
 
 export default function Users() {
-  const { data, error } = useSWR("/users");
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const { data } = useSWR(`/users?searchTerm=${searchTerm}`);
+
   return (
-    <div className="place-items-center p-20">
+    <div className="flex flex-col gap-12 p-24">
+      <TextField
+        className="w-full"
+        id="input-search"
+        label="Search users"
+        variant="standard"
+        onChange={(i) => setSearchTerm(i.target.value)}
+      />
+
       <div className="grid grid-cols-5 place-items-start justify-center gap-10">
         {data?.map((user: IUserCard) => (
           <UserCard key={user.id} user={user} />
